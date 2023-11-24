b4c003b04b300e8a4132ffad176808d31b84bea3
joka63 <JoKatzer@gmx.de>
2023-11-24 11:35:17 +0100

    test: remove obsolete unit tests

--- tests/convert.tiddly.test.ts
+++ tests/convert.tiddly.test.ts
@@ -1,6 +1,7 @@
 import { convertTiddlyWikiToMarkdown } from "../parser/TiddlyWikiToMarkdown"
 import { convertMarkdownToTiddlyWiki } from "../parser/MarkdownToTiddlyWiki"
-import { TiddlyToMarkdownTestData } from "./TiddlyWikiToMarkdown.test";
+
+export type TiddlyToMarkdownTestData = { label: string, tiddlerText: string, expectedMarkdown: string };
 
 export const markdownTestData: TiddlyToMarkdownTestData[] = [
     /** 
@@ -37,13 +38,25 @@ export const markdownTestData: TiddlyToMarkdownTestData[] = [
         tiddlerText: "Two images: [img[Logo.png]] and external [img[https://tiddlywiki.com/favicon.ico]]",
         expectedMarkdown: "Two images: ![[Logo.png]] and external ![https://tiddlywiki.com/favicon.ico](https://tiddlywiki.com/favicon.ico)"
     },
+    {   label: "Code blocks",
+        tiddlerText: "```\nThis is a code block\n* Don't convert this to a list\n# A Comment\n! Not a header\n```",
+        expectedMarkdown: "```\nThis is a code block\n* Don't convert this to a list\n# A Comment\n! Not a header\n```",
+    },
     {   label: "Headings",
         tiddlerText: `! This is a level 1 heading\n!! This is a level 2 heading`,
         expectedMarkdown: `# This is a level 1 heading\n## This is a level 2 heading`
     },
-    {   label: "Headings",
-        tiddlerText: `! One\n!! Two`,
-        expectedMarkdown: `# One\n## Two`
+    {   label: "Lists",
+        tiddlerText: `* First list item\n* Second list item\n** A subitem\n* Third list item`,
+        expectedMarkdown: `- First list item\n- Second list item\n  - A subitem\n- Third list item`
+    },
+    {   label: "Indented lists",
+        tiddlerText: `* Test CTA\n** Lead magnet\n*** Thank you page`,
+        expectedMarkdown: `- Test CTA\n  - Lead magnet\n    - Thank you page`
+    },
+    {   label: "Numbered lists",
+        tiddlerText: `A numbered list:\n# First list item\n# Second list item\n## A subitem\n# Third list item`,
+        expectedMarkdown: `A numbered list:\n1. First list item\n1. Second list item\n   1. A subitem\n1. Third list item`
     },
 ];
 
