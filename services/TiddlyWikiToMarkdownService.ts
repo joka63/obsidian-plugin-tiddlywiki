a6676f65ba80e3f898412b9553cab3c4997d663e
joka63 <JoKatzer@gmx.de>
2023-11-25 21:15:51 +0100

    feat: extend convertTiddlersToObsidianMarkdown
    Add optional name of a table-of-contents tiddler to trigger
    generation of sub-folders for converted tiddlers

--- services/TiddlyWikiToMarkdownService.ts
+++ services/TiddlyWikiToMarkdownService.ts
@@ -13,7 +13,9 @@ export interface Tiddler {
 
 export type ObsidianMarkdown = {
 	title: string;
-	content: string
+	content: string;
+	filename?: string;
+	tags?: string[];
 }
 
 export async function convertJSONToTiddlers(file: File): Promise<Tiddler[]> {
@@ -31,7 +33,13 @@ export async function convertJSONToTiddlers(file: File): Promise<Tiddler[]> {
 	});
 }
 
-export function convertTiddlersToObsidianMarkdown(tiddlers: Tiddler[]) {
+/**
+ * Converts tiddlers to Obsidian Markdown. If toc_name is provided, it will be used as the title of table of contents tiddler.
+ * @param tiddlers 
+ * @param toc_name 
+ * @returns an array of ObsidianMarkdown objects, each containing the title, content, and filename of a tiddler
+ */
+export function convertTiddlersToObsidianMarkdown(tiddlers: Tiddler[], toc_name?: string) {
 	const markdownArray: ObsidianMarkdown[] = [];
 
 	for (const tiddler of tiddlers) {
@@ -40,20 +48,39 @@ export function convertTiddlersToObsidianMarkdown(tiddlers: Tiddler[]) {
 			+ `---\n`;
 
 		const content = frontMatter + convertTiddlyWikiToMarkdown(tiddler.text);
+		const filename = `${tiddler.title}.md`.replace(/[\/\:\\]/g, '');
 
 		markdownArray.push({
 			content,
-			title: tiddler.title
+			title: tiddler.title,
+			filename,
+			tags: tiddler.tags?.split(' ')
 		});
 	}
 	return markdownArray;
+}
+
+/**
+ * Gets a list of directories containing markdown files from the filenames in the markdownArray.
+ * @param markdownArray 
+ * @returns a list of directories containing markdown files
+ * @throws an error if the directory does not exist
+ */
+export function markdownArrayDirectories(markdownArray: ObsidianMarkdown[]): string[] {
+	let directorySet = new Set<string>();
+	for (const markdownFile of markdownArray) {
+		if (!markdownFile.filename) continue;
+		const directory = path.dirname(markdownFile.filename).split('/')[0];
+		if (directory) directorySet.add(directory);
+	}
+	return Array.from(directorySet);
 }
 
 export async function writeObsidianMarkdownFiles(markdownArray: ObsidianMarkdown[], directoryPath: string) {
 	fs.mkdirSync(directoryPath, { recursive: true });
 
 	for (const markdownFile of markdownArray) {
-		const fileName = `${markdownFile.title}.md`.replace(/[\/\:\\]/g, '');
+		const fileName = markdownFile.filename || `${markdownFile.title}.md`.replace(/[\/\:\\]/g, '');
 
 		fs.writeFileSync(path.join(directoryPath, fileName), markdownFile.content, 'utf-8');
 	}
